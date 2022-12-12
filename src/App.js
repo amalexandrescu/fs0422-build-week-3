866ed0187ee77f2d2705f9723c49e8205be06bf8
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-12-12 16:17:56 +0200

    ResourcesComponent created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ResourcesComponent from "./components/ResourcesComponent.jsx";
+import ResourcesSinglePageComponent from "./components/ResourcesSinglePageComponent.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ResourcesComponent />} />
+          <Route
+            path="/details/resources"
+            element={<ResourcesSinglePageComponent />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
