57e6283c6d2f766e7298a82601f8e22388b04e07
annoinspace <aneesahalmask@gmail.com>
2022-12-12 09:55:44 +0000

    initial commit

---
+++ src/components/MyProfile/MyProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import SideComponentsMyProfile from "./SideComponentsMyProfile"
+
+export default function MyProfile() {
+  return (
+    <>
+      <div>MyProfile</div>
+      <SideComponentsMyProfile />
+    </>
+  )
+}
---
+++ src/components/MyProfile/SideComponentsMyProfile.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SideComponentsMyProfile() {
+  return <div>SideComponentsMyProfile</div>
+}
