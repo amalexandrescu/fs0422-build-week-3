beca76ff11b03ba5f7eb30d044ef89ad2bccaff7
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-12-12 11:12:13 +0200

    redux boilerplate created

---
+++ src/redux/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+// configureStore will set up the Redux Store for us!
+
+const bigReducer = combineReducers({
+  // cart: cartReducer,
+});
+
+const store = configureStore({
+  reducer: bigReducer, // here there's place for just 1 value!
+});
+
+export default store;
+
+// now the store is ready! let's INJECT IT into our REACT APP!
+// we do it in the src/index.js file
