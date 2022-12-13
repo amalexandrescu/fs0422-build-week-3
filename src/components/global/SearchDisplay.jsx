5e8c95fbcdd08cc3b91e7248f06521c721cb8501
annoinspace <aneesahalmask@gmail.com>
2022-12-13 13:19:57 +0000

    styling changes

--- src/components/global/SearchDisplay.jsx
+++ src/components/global/SearchDisplay.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { useSelector } from "react-redux"
-
+import { BiSearch } from "react-icons/bi"
+import { BsDot } from "react-icons/bs"
 export default function SearchDisplay() {
   let usersFromSearch = useSelector(
     (state) => state.usersFromSearchFilter.searchResults
   )
+
+  let firstTen = usersFromSearch.slice(0, 10)
   console.log("usersFromSearch", usersFromSearch)
 
   return (
@@ -13,10 +16,19 @@ export default function SearchDisplay() {
         <div>Search for users</div>
       ) : (
         <>
-          {usersFromSearch.map((user) => (
-            <div key={user._id}>
-              <span>{user.name}</span>
-              <span> {user.surname}</span>
+          {firstTen.map((user) => (
+            <div key={user._id} className="search-individual-user-diplay">
+              <span className="mr-3">
+                <BiSearch />
+              </span>
+              <span>{user.name} </span>
+              <span>{user.surname}</span>
+              <span>
+                <BsDot />
+              </span>
+              <span className="recommended-user-job-description-text">
+                {user.title}
+              </span>
             </div>
           ))}
         </>
