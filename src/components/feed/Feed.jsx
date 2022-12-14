79cff0dec6f1e1dcfebccf7faa8ac0c4dd9a9283
AmusedAlex <alex.spomer74@gmail.com>
2022-12-14 15:18:09 +0100

    Updates on Feed structure

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,22 +1,25 @@
 import { Col, Container, Row } from "react-bootstrap";
+import Center from "./Center";
+import Leftside from "./Leftside";
+import Rightside from "./Rightside";
 
 const Feed = () => {
   return (
     <Container className="mt-4">
       <Row>
         <Col xs={3} className={"pr-4 pl-0"}>
-          <div className="" style={{ backgroundColor: "slategrey" }}>
-            Here goes the Leftside
+          <div className="">
+            <Leftside />
           </div>
         </Col>
         <Col xs={6} className={"pr-4 pl-0"}>
           <div style={{ backgroundColor: "slategrey" }}>
-            Here goes the CENTER
+            <Center />
           </div>
         </Col>
         <Col xs={3} className={"px-0"}>
           <div style={{ backgroundColor: "slategrey" }}>
-            Here goes the Rightside
+            <Rightside />
           </div>
         </Col>
       </Row>
---
+++ src/components/feed/Leftside.jsx
@@ -0,0 +1,11 @@
+const LeftSide = () => {
+  return (
+    <div>
+      <div>FeedUserDetails</div>
+      <div>FeedCompanyDetails</div>
+      <div>FeedRecentHashtags</div>
+    </div>
+  );
+};
+
+export default LeftSide;
