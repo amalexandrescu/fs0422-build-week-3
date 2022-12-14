70427015a94fb1b8ca38aadaa558c4e850c13650
annoinspace <aneesahalmask@gmail.com>
2022-12-14 21:29:28 +0000

    user post details

--- src/components/feed/MainFeedSectionWithPosts.jsx
+++ src/components/feed/MainFeedSectionWithPosts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { getFeedPostsAction } from "../../redux/actions"
+import FeedPostLike from "./FeedPostLike"
 
 export default function MainFeedSectionWithPosts() {
   const allFeedPosts = useSelector((state) => state.feedPosts.feedPostArray)
@@ -25,9 +26,13 @@ export default function MainFeedSectionWithPosts() {
       {allFeedPosts && (
         <>
           {latestPostSlice.map((post) => (
-            <div key={post._id} className="feed-post border p-feed">
-              {post.text}
+            <div key={post._id} className="feed-post border p-feed pb-1">
+              <div className=" border-top mr-2 ml-2">
+                <p className="mt-3 mb-3"> {post.text}</p>
+              </div>
+
               {console.log(post.text.length)}
+              <FeedPostLike />
             </div>
           ))}
         </>
