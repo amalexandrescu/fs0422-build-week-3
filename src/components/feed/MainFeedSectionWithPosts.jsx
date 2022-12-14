d3377e8177e0f67f7c7d039a759af3e50c2673e3
annoinspace <aneesahalmask@gmail.com>
2022-12-14 22:07:03 +0000

    more css fixes

--- src/components/feed/MainFeedSectionWithPosts.jsx
+++ src/components/feed/MainFeedSectionWithPosts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-
+import { BsThreeDots } from "react-icons/bs"
+import placeholder from "../../assets/v-team-logo.png"
 import { getFeedPostsAction } from "../../redux/actions"
 import FeedPostLike from "./FeedPostLike"
 
@@ -10,7 +11,7 @@ export default function MainFeedSectionWithPosts() {
   const allLatestPosts = allFeedPosts.slice(0).reverse()
   //   filtering out short posts
   const filteredPosts = allLatestPosts.filter((post) => post.text.length > 25)
-  console.log("filtered length", filteredPosts.length)
+
   // getting 20 posts from the remaining array
   const latestPostSlice = filteredPosts.slice(0, 30)
   console.log("-----------latest 30 posts---------", latestPostSlice)
@@ -27,11 +28,37 @@ export default function MainFeedSectionWithPosts() {
         <>
           {latestPostSlice.map((post) => (
             <div key={post._id} className="feed-post border p-feed pb-1">
+              <div className="d-flex justify-content-between mr-2 ml-2">
+                <div></div>
+                <div>
+                  <BsThreeDots />
+                </div>
+              </div>
               <div className=" border-top mr-2 ml-2">
+                <div className="mt-3 d-flex ">
+                  <div className="border recommended-user-image">
+                    <img src={post.user.image} alt="" />
+                  </div>{" "}
+                  <div className="feed-text-user-wrapper">
+                    <div className="mb-0 small-header-text bolder feed-text-name">
+                      <span>{post.user.name}</span>
+                      <span>{post.user.surname}</span>
+                    </div>
+                    <div className="mb-1 small-height">
+                      <span className="feed-post-tiny-text truncate-text">
+                        {post.user.title}
+                      </span>
+                    </div>
+                    <div className="mb-1 small-height">
+                      <span className="feed-post-tiny-text  ">
+                        {post.user.createdAt}
+                      </span>
+                    </div>
+                  </div>
+                </div>
                 <p className="mt-3 mb-3"> {post.text}</p>
               </div>
 
-              {console.log(post.text.length)}
               <FeedPostLike />
             </div>
           ))}
